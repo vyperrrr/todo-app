4285fafdbaa0ead99459a5b4cdda6cda7d0e0f7c
Gerzsenyi Patrik <78800814+vyperrrr@users.noreply.github.com>
2023-06-30 22:08:36 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+
+function TodoList({ todos, onRemove }) {
+  return (
+    <>
+      {todos.map((todo, index) => (
+        <Todo key={index} todo={todo} index={index} onRemove={onRemove} />
+      ))}
+    </>
+  );
+}
+
+function Todo({ todo, index, onRemove }) {
+  const handleCheckboxChange = () => {
+    onRemove(index);
+  };
+
+  return (
+    <div>
+      <input type="checkbox" onChange={handleCheckboxChange} />
+      <label>{todo}</label>
+    </div>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    if (inputRef.current.value !== "") {
+      setTodos([...todos, inputRef.current.value]);
+      inputRef.current.value = "";
+    }
+  };
+
+  function handleRemove(index) {
+    setTodos((prevTodos) => prevTodos.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} onRemove={handleRemove} />
+      <input type="text" ref={inputRef} />
+      <button onClick={handleClick}>Add Todo</button>
     </div>
   );
 }
